300db77e8ca8fc22097692fd84570b1a714ec2d9
SinuheJH <sinuhe.jh@gmail.com>
2022-10-13 11:34:48 -0500

    añadimos sublista, como ejemplo

---
+++ src/SublistMethod.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SublistMethod {
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> arrList = new ArrayList<>();
+            arrList.add("A");
+            arrList.add("B");
+            arrList.add("C");
+            arrList.add("D");
+
+            System.out.println("original arrList = " + arrList);
+            List<String> sublista = arrList.subList(0,4);
+            System.out.println("sublista = " + sublista);
+
+            int listSize = arrList.size();
+            System.out.println("listSize = " + listSize);
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("e = " + e);
+        } catch (IllegalArgumentException e) {
+            System.out.println("e = " + e);
+        }
+    }
+}
